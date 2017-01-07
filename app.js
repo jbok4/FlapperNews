57a3ae908a04f88ec35e3e8ae66bbd76a4cf22f2
jbok4 <jbok4@aol.com>
2017-01-06 22:43:03 -0500

    added some partials and ui-router but not functioning yet

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 angular
 
-.module('flapperNews', [])
+.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}])
+
 .factory('posts', [function(){
   var vm = {
     posts: []
@@ -20,7 +40,11 @@ angular
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link, 
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
       $scope.title = '';
       $scope.link = '';
@@ -30,4 +54,23 @@ angular
       post.upvotes += 1;
     };
 
+  }])
+
+  .controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  
+
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+    $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  }
   }]);
